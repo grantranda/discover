097aaf284490fd8193730560af3119a99a4978e1
Grant Randa <48031522+grantranda@users.noreply.github.com>
2022-09-03 02:15:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 import {Hop} from "@onehop/js";
 import {hop} from "@onehop/client";
 
+const channelId = "channel_NTAzODYxNTgwMjY2NDE0MjM";
 const myToken = "ptk_c181NDNlZGUzZGM2ZmM1YTcxYzM0MWRkOGYyZTlkYzE2N181MDM4MTc3OTIyNjUzMzkxMw";
-
 const hopServer = new Hop(myToken);
-hopServer.channels.publishMessage()
+hopServer.channels.publishMessage(
+    channelId,
+    "MESSAGE_CREATE",
+    {
+        content: "Test Message",
+        author_name: "Grant"
+    }
+)
 
+// const projectId = "project_NTAzODA4NjA2NDA0MDM0Nzc";
 //
-
-const projectId = "project_NTAzODA4NjA2NDA0MDM0Nzc";
-
-let project = hop.init({
-    projectId: projectId
-})
-project.subscribeToChannel()
+// let project = hop.init({
+//     projectId: projectId
+// })
+// project.subscribeToChannel()
 
 
 // // Example: Creating a project secret
