f299d63b932224f016c32fb7cc87438df29b6e81
Grant Randa <grantranda@gmail.com>
2022-09-03 17:11:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import {ChannelType, Hop} from "@onehop/js";
+import {WebSocketServer} from "ws";
+import http from "http";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -9,17 +11,30 @@ const projectToken = "ptk_c181NDNlZGUzZGM2ZmM1YTcxYzM0MWRkOGYyZTlkYzE2N181MDM4MT
 export const hop = new Hop(projectToken)
 
 const app = express();
-const ads = [
-    {title: "Hello world!"}
-];
+const server = http.createServer();
+const wss = new WebSocketServer({server: server})
 
-console.log("Server started")
+server.on("request", app);
+wss.on("connection", (ws) => {
+    console.log("Connection");
+
+    ws.on("message", (message) => {
+        console.log("Received:", message);
+
+        ws.send(JSON.stringify({
+            title: "Send message"
+        }))
+    });
+});
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined"));
 
+const ads = [
+    {title: "Hello world!"}
+];
 app.get("/", (req, res) => {
     res.send(ads);
 });
@@ -35,30 +50,45 @@ app.get("/multiplayer/create", async (req, res) => {
     ]);
 });
 
-app.get("/multiplayer/join", async (req, res) => {
-    res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Connection", "keep-alive");
-    res.flushHeaders();
 
-    res.on("close", () => {
-        console.log("Connection closed");
-        res.end();
-    });
 
-    res.write(`title: ${JSON.stringify("Joined")}`)
+// app.get("/multiplayer/join", (req, res) => {
+//     // res.setHeader("Cache-Control", "no-cache");
+//     // res.setHeader("Content-Type", "text/event-stream");
+//     // res.setHeader("Access-Control-Allow-Origin", "*");
+//     // res.setHeader("Connection", "keep-alive");
+//     res.set({
+//         "Content-Type": "text/event-stream",
+//         "Cache-Control": "no-cache",
+//         "Connection": "keep-alive",
+//         "Access-Control-Allow-Origin": "*"
+//     });
+//     // res.flushHeaders();
+//
+//     res.write("title: Join")
+//     // res.on("", () => {
+//     //     console.log("Connection opened");
+//     // });
+//     res.on("close", () => {
+//         console.log("Connection closed");
+//         res.end();
+//     });
+//
+//     // res.write(`title: ${JSON.stringify("Joined")}`)
+//
+//     // while (true) {
+//     //     // await new Promise(resolve => setTimeout(resolve, 1000))
+//     //     res.write(JSON.stringify([
+//     //         {
+//     //             title: "Multiplayer - Join"
+//     //         }
+//     //     ]));
+//     // }
+// });
 
-    // while (true) {
-    //     // await new Promise(resolve => setTimeout(resolve, 1000))
-    //     res.write(JSON.stringify([
-    //         {
-    //             title: "Multiplayer - Join"
-    //         }
-    //     ]));
-    // }
-});
-
-app.listen(3001, "0.0.0.0", 511, () => {
+server.listen(3001, "0.0.0.0", 511, () => {
     console.log("Listening on port 3001");
 });
+// app.listen(3001, "0.0.0.0", 511, () => {
+//     console.log("Listening on port 3001");
+// });
